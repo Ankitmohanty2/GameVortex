2e3ea174d82d1d1b3052bdb584d6dafb4d1e7087
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-02-26 17:25:55 +0530

    updated to closer

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    
+    <h1>Hello Baby</h1>
   </React.StrictMode>,
 )
